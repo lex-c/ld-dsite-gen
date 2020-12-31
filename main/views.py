d1422de65f9e79de9d7bf827d2f00b5fc237b1fe
lex-c <chlochlo.lexa@gmail.com>
2020-12-31 10:28:53 -0500

    add buildpack

--- main/views.py
+++ main/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
+import requests
+import json
+import firebase_admin
 
 # Create your views here.
 
+from google.analytics.data import AlphaAnalyticsDataClient
+from google.analytics.data_v1alpha.types import DateRange
+from google.analytics.data_v1alpha.types import Dimension
+from google.analytics.data_v1alpha.types import Entity
+from google.analytics.data_v1alpha.types import Metric
+from google.analytics.data_v1alpha.types import RunReportRequest, RunRealtimeReportRequest
+
+PROPERTY_ID = "properties/257302691"
+default_app = firebase_admin.initialize_app()
+
+
+
 def index(request):
-    return render(request, 'main/react-front/build/index.html')
+  client = AlphaAnalyticsDataClient()
+  g_request = RunRealtimeReportRequest(
+                            property=PROPERTY_ID,
+                            dimensions=[Dimension(name='eventName')],
+                            metrics=[Metric(name='eventCount')],
+                            limit=-1
+                            )
+  response = client.run_realtime_report(g_request)
+  print('results:')
+  for row in response.rows:
+      print(row.dimension_values[0].value, row.metric_values[0].value)
+  return render(request, 'main/react-front/build/index.html')
